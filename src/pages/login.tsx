caa88d542a22f33c4452b05268776154d6a0c2c3
Erwins T. Milord <emilord@mdsol.com>
2022-08-23 10:05:27 -0400

    Add api call to AWS lambda service and also allow for the state to disable a button call

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 import {Button} from "../components/general/button";
+import axios from "axios";
 
 const Login: React.FC<{}> = () => {
     const [username, setUsername] = useState<string | null>(null)
     const [password, setPassword] = useState<string | null>(null)
-    const onLogin = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const onLogin = async () => {
         console.log(username, password);
+        setIsLoading(true);
+        const result = await axios.post(
+            'https://58iiwrc1gf.execute-api.us-west-1.amazonaws.com/prod/login',
+            { username, password },
+            {
+                headers: {
+                    'x-api-key': 'TJbulKjIHU7VxpjDVGARI6mc3TgpWuMs3GId6r6i',
+                }
+            }
+        );
+        setIsLoading(false);
+        console.log(result);
     };
+
+    // Option 2 change the layout if the page is loading
+    /**
+    if (isLoading) {
+        return <div>...Logging you in</div>;
+    }
+     */
 
   return (
       <div>
           <input type={'text'} name="username" onChange={(e) => setUsername(e.target.value)}/>
           <input type={'password'} name="password" onChange={(e) => setPassword(e.target.value)}/>
-          <Button onClick={onLogin}>Login NOW!</Button>
+          <Button disabled={isLoading} onClick={onLogin}>Login NOW!</Button>
       </div>
   );
 };
